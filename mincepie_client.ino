7214a847d2d4af544dc37293c2d0399edd75c12e
Alex Forey <me@alexforey.com>
2012-11-22 17:26:10 +0000

    Connects using Static IPs to the local network, no DNS

--- mincepie_client.ino
+++ mincepie_client.ino
@@ -16,10 +16,11 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = {  0x90, 0xA2, 0xDA, 0x09, 0x00, 0x03 };
-IPAddress server(173,194,33,104); // Google
+// Network Settings
+
+byte mac[] = {  0x90, 0xA2, 0xDA, 0x09, 0x00, 0x03 }; // MAC Found on sticker
+IPAddress server(10,0,1,8); // Google
+IPAddress ip(10,0,1,105); // My static local IP
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server 
@@ -29,20 +30,25 @@ EthernetClient client;
 void setup() {
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
+  
+  Serial.println("Connected to Serial");
 
   // start the Ethernet connection:
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    for(;;)
-      ;
-  }
+  Ethernet.begin(mac, ip);
   // give the Ethernet shield a second to initialize:
   delay(1000);
   Serial.println("connecting...");
+  
+  
+ // print your local IP address:
+  Serial.print("My IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print("."); 
+  }
+  
+  Serial.println();
 
   // if you get a connection, report back via serial:
   if (client.connect(server, 80)) {
