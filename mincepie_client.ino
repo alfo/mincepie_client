6c4902aa2b587e2ad3c24aaf02d01181050a9137
Alex Forey <me@alexforey.com>
2012-11-22 18:37:48 +0000

    LEDs and this and that

--- mincepie_client.ino
+++ mincepie_client.ino
@@ -19,9 +19,16 @@
 // Network Settings
 
 byte mac[] = {  0x90, 0xA2, 0xDA, 0x09, 0x00, 0x03 }; // MAC Found on sticker
-byte server[] = {173 , 194 , 41 , 169 }; // Google
-byte ip[] = { 10 , 0 , 1 , 105 }; // My static local IP
-byte dns[] = { 10 , 0 , 1 , 1 }; // DNS on the local network
+IPAddress ip(10, 0, 1, 105);
+IPAddress gateway(10, 0, 1, 1);
+byte server[] = {10 , 0 , 1 , 8 }; // Google
+
+// LED Settings
+int rLED = 3;
+int gLED = 5;
+int bLED = 6;
+
+int buttonState = 0;
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server 
@@ -29,13 +36,19 @@ byte dns[] = { 10 , 0 , 1 , 1 }; // DNS on the local network
 EthernetClient client;
 
 void setup() {
+  // Set up RGB LED
+  pinMode(rLED, OUTPUT);
+  pinMode(gLED, OUTPUT);
+  pinMode(bLED, OUTPUT);
+  pinMode(2, INPUT);
+  
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
   
   Serial.println("Connected to Serial");
 
   // start the Ethernet connection:
-  Ethernet.begin(mac, ip, thedns);
+  Ethernet.begin(mac, ip);
   // give the Ethernet shield a second to initialize:
   delay(1000);
   Serial.println("connecting...");
@@ -50,38 +63,60 @@ void setup() {
   }
   
   Serial.println();
+  
+  setLED(160, 255, 97); // Set the LED to begin with
+  
+  digitalWrite(2, HIGH);
 
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    Serial.println("connected");
-    // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.0");
-    client.println();
-  } 
-  else {
-    // kf you didn't get a connection to the server:
-    Serial.println("connection failed");
-  }
 }
 
 void loop()
 {
-  // if there are incoming bytes available 
-  // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
-
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-
-    // do nothing forevermore:
-    for(;;)
-      ;
+  if (handleButton()) {
+    setLED(255,100,0);
+    eat();
+    setLED(160, 255, 97);
+    delay(5000);
   }
 }
 
+void eat() {
+  if (client.connect(server, 80)) {  //starts client connection, checks for connection
+    Serial.println("connected");
+    client.println("GET /mincepie/eat.php HTTP/1.0"); //download text
+    client.println(); //end of get request
+  } 
+  else {
+    Serial.println("connection failed"); //error message if no client connect
+    Serial.println();
+  }
+
+  while(client.connected() && !client.available()) delay(1); //waits for data
+// add counter as an attempt to bail out if too much input comes in
+//
+  int counter = 0;
+  while (client.connected() || client.available()) { //connected or data available
+    char c = client.read(); //gets byte from ethernet buffer
+    Serial.print(c); //prints byte to serial monitor 
+    counter = counter + 1;
+    if (counter >= 500 ) {
+      exit;  
+    }
+  }
+  Serial.println();
+  Serial.println("disconnecting.");
+  Serial.println("==================");
+  Serial.println();
+  client.stop(); //stop client
+}
+
+void setLED(int r, int g, int b) {
+  analogWrite(rLED, 255-r);
+  analogWrite(gLED, 255-g);
+  analogWrite(bLED, 255-b);
+}
+
+boolean handleButton() {
+  int buttonPressed = !digitalRead(2);
+  return buttonPressed;
+}
